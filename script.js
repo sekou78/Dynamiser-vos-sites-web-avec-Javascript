c7fe2524e28a8eb6e9b600b00bf858e45cc342b7
Sekou CISSE <s.cisse78@gmail.com>
2023-06-01 22:53:32 +0200

    hold
    Co-authored-by: Kurtozorus <Kurtozorus@users.noreply.github.com>

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ let images = [
 ];
 
 let dice = document.querySelectorAll("img");
+let roundValue = document.getElementById('ROUND1')
+let a;
+let zGlobal = document.getElementById('GLOBAL1')
+function switchPlayer(Global) {
+  
+}
 
 function roll() {
   dice.forEach(function(die){
@@ -17,14 +23,32 @@ function roll() {
     dice.forEach(function(die){
       die.classList.remove("shake");
     });
-
+    
     let dieValue = Math.floor(Math.random() * 6);
-
+    
     console.log((dieValue) + 1);
     document.querySelector("#die").setAttribute("src", images[dieValue]);
-    document.querySelector("#ROUND1").innerHTML = (dieValue + 1);
+    // document.querySelector("#ROUND1").innerHTML = (dieValue + 1);
+    a = ((dieValue) + 1)
+    roundValue.innerHTML = a;
   },
   1000
   );
-  
+  // while(player)
+  // if(a = 1) {
+  //   a = 0;
+  // }
 }
+let GLOBALE1 = 0 ;
+function hold() {
+    GLOBALE1 += a;
+    zGlobal.innerHTML = GLOBALE1;
+    console.log(GLOBALE1);
+}
+
+
+
+
+
+
+
