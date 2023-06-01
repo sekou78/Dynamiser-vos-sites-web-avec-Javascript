9d23d676d2a038de901ec001bc92032b8ebcdb73
Sekou CISSE <s.cisse78@gmail.com>
2023-06-01 17:47:37 +0200

    player1 debut

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-//images/dice-01.png 
 let images = [
-  "/images/dice-01.png",
-  "/images/dice-02.png",
-  "/images/dice-03.png",
-  "/images/dice-03.png",
+  "images/dice-01.png",
+  "images/dice-02.png",
+  "images/dice-03.png",
+  "images/dice-04.png",
+  "images/dice-05.png",
+  "images/dice-06.png"
 ];
+
+let dice = document.querySelectorAll("img");
+
+function roll() {
+  dice.forEach(function(die){
+    die.classList.add("shake");
+  });
+  setTimeout(function(){
+    dice.forEach(function(die){
+      die.classList.remove("shake");
+    });
+
+    let dieValue = Math.floor(Math.random() * 6);
+
+    console.log((dieValue) + 1);
+    document.querySelector("#die").setAttribute("src", images[dieValue]);
+    document.querySelector("#ROUND1").innerHTML = (dieValue + 1);
+  },
+  1000
+  );
+  
+}
