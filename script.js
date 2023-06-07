20c8a48e4b9d8bc88dbada8767b7f091c7fb399b
Sekou CISSE <s.cisse78@gmail.com>
2023-06-07 20:31:30 +0200

    playActive

--- script.js
+++ script.js
@@ -23,7 +23,15 @@ let GLOBALE2 = 0;
 let tour = 0;
 //let endGame;
 
-
+function playActive(){
+  if(tour === 0) {
+    active1.classList.add("show-reponse")
+    active2.classList.add("hidden-reponse")
+  }else {
+    active1.classList.add("hidden-reponse")
+    active2.classList.add("show-reponse")
+  }
+}
 function roll() {
   dice.forEach(function (die) {
     die.classList.add("shake");
@@ -33,20 +41,22 @@ function roll() {
       die.classList.remove("shake");
     });
     let dieValue = Math.floor(Math.random() * 6);
-    
+
     console.log(dieValue + 1);
     document.querySelector("#die").setAttribute("src", images[dieValue]);
     // document.querySelector("#ROUND1").innerHTML = (dieValue + 1);
     valeurDuDés = (dieValue + 1);
-    
+
     //affichage du tour/round(Dé)
     if (tour == 0) {
       // active2.style.display = "none";
+      playActive()
       GLOBALROUND1 += valeurDuDés;
       roundValue.innerHTML = GLOBALROUND1;
       roundValue2.innerHTML = 0;
     } else {
       // active1.style.display = "none";
+      playActive()
       GLOBALROUND2 += valeurDuDés;
       roundValue2.innerHTML = GLOBALROUND2;
       roundValue.innerHTML = 0;
@@ -57,16 +67,23 @@ function roll() {
       tour++;
       valeurDuDés = 0;
       GLOBALROUND1 = 0;
+      playActive()
     } else if (valeurDuDés == 1 && tour == 1) {
       tour--;
       valeurDuDés = 0;
       GLOBALROUND2 = 0;
+      playActive()
     }
   }, 1000);
 }
 
 function hold() {
   if (tour == 0) {
+    // active2.style.visibility = "hidden";
+    // active2.style.opacity = 0 ;
+    // active1.style.visibility = "visible";
+    // active1.style.opacity = 1 ;
+    playActive()
     GLOBALE1 += GLOBALROUND1;
     zGlobal.innerHTML = GLOBALE1;
     console.log(GLOBALE1);
@@ -75,6 +92,11 @@ function hold() {
     GLOBALROUND1 = 0;
     tour++;
   } else {
+    // active1.style.visibility = "hidden";
+    // active1.style.opacity = 0;
+    // active2.style.visibility = "visible";
+    // active2.style.opacity = 1;
+    playActive()
     GLOBALE2 += GLOBALROUND2;
     zGlobal2.innerHTML = GLOBALE2;
     console.log(GLOBALE2);
@@ -85,26 +107,28 @@ function hold() {
   }
 }
 
-
+// New game
 function endGame() {
   if (endGame) {
+    active1.classList.add("show-reponse")
+    active2.classList.add("hidden-reponse")
     GLOBALE1 = 0;
     zGlobal.innerHTML = 0;
     valeurDuDés = 0;
     roundValue.innerHTML = 0;
     GLOBALROUND1 = 0;
-  } else if (endGame) {
     GLOBALE2 = 0;
     zGlobal2.innerHTML = 0;
     valeurDuDés = 0;
     roundValue2.innerHTML = 0;
     GLOBALROUND2 = 0;
+  } else {
   }
 }
 
 // endGame = document.addEventListener("click", () => {
 
-// }); 
+// });
 
 
 // newGame().addEventListener('click',()=> {
@@ -118,4 +142,3 @@ function endGame() {
 //     roundValue.innerHTML = 0;
 //     roundValue2.innerHTML = 0;
 //     }
-  
